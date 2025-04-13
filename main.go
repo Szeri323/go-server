7c5af3f696906e91bc8ded131a20f2b6eeeb24af
Szeri <szeri323@gmail.com>
2025-04-13 15:29:35 +0200

    add routing, decopled architecture, admin routes, traffic and server health analusis

--- main.go
+++ main.go
@@ -1,21 +1,72 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-type ServeMux struct {
+// handler func(http.ResponseWriter, *http.Request)
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) printHits() []byte {
+	hits := cfg.fileserverHits.Load()
+	hitStr := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", hits)
+	return []byte(hitStr)
+}
+
+func (cfg *apiConfig) resetHits() {
+	cfg.fileserverHits.Store(0)
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(cfg.printHits())
+}
+
+func (cfg *apiConfig) handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.resetHits()
 }
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	serveMux := http.NewServeMux()
-	server := http.Server{
-		Addr:    ":8080",
+
+	var apiCfg apiConfig
+	fileServer := http.FileServer(http.Dir(filepathRoot))
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+	serveMux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	serveMux.HandleFunc("GET /api/healthz", apiCfg.handlerHealthz)
+	serveMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+
+	server := &http.Server{
+		Addr:    ":" + port,
 		Handler: serveMux,
 	}
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatalf("error could not start server: %v", err)
 	}
+
 }
